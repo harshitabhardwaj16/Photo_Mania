a198cf908f0093574b8cc2432ab27be5b1adac04
harshitabhardwaj16 <harshita.bhardwaj_cs18@gla.ac.in>
2020-10-14 22:43:32 +0530

    Photo_Mania

---
+++ js/grayscale.js
@@ -0,0 +1,39 @@
+var canvas;
+var newCanva;
+var image;
+var img;
+function upload(){
+    canvas = document.getElementById("d1");
+    img = document.getElementById("f");
+    image = new SimpleImage(img);
+    image.drawTo(canvas);
+}
+function isImageLoaded(aImage) {
+    if(aImage==null || !aImage.complete()) {
+        alert("You need to load image first!");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+function greyScale(){
+    if(isImageLoaded(image)){
+        for(var pix of image.values()){
+            var avg = (pix.getGreen() + pix.getRed() + pix.getBlue() )/ 3;
+            pix.setRed(avg);
+            pix.setBlue(avg);
+            pix.setGreen(avg);
+        }
+        newCanva = document.getElementById("can");
+        image.drawTo(newCanva);
+    }
+}
+function reset(){
+    if(isImageLoaded(image)){
+        var can = document.getElementById("can");
+        var img = document.getElementById("f");
+        var im = new SimpleImage(img);
+        im.drawTo(can);
+    }
+}
---
+++ script.js
@@ -0,0 +1,51 @@
+// Contain
+document.addEventListener("DOMContentLoaded", function () {
+
+    // All JavaScript
+    /* Pop up window */
+    // let magic = prompt("Please type a number from 1 to 10");
+    alert("Thank you for visiting Pixel Plus ...!!");
+  
+  
+    // Site Header - Navigation Bar (Smooth Scrolling to sections)
+    // Make sure to add Jquery link to html
+    /* Reference: https://www.w3schools.com/howto/howto_css_smooth_scroll.asp */
+    /* Reference: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+  
+    // Add the smooth scrolling to all links
+    // Select all links with hashes
+    $('a[href*="#"]')
+      //remove useless links :D 
+      .not('[href="#"]')
+      .not('[href="#0"]')
+    $("a").on('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        // Use jQuery Function: animate() to add smooth scrolling
+        // The number below identifies the time (in milliseconds) for scrolling
+        // Identify the HTML Element to animate()
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1600, function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        })
+      }
+    });
+    // add notifiction display:flex when cart is marked
+  
+    // Section 5 - Subscription Form Email Validation
+  
+    let messageForm = document.getElementById("signup");
+    const input = document.getElementById('signup-email');
+    messageForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+      const trimVal = input.value.trim();
+      if (trimVal == "") {
+        alert("Please enter some text");
+      } else {
+        alert("Thank you for your subscription!");
+      }
+    });
+  });
